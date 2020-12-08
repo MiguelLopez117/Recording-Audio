eda58cf9768e1f7e2e3ce2b526100c01cd53e73b
Miguel <mlopez38@unm.edu>
2020-12-08 16:33:33 -0700

    Updated Stereo-Mono

--- Stereo-Mono.py
+++ Stereo-Mono.py
@@ -9,6 +9,8 @@ a = AudioSegment.from_file("delme_rec_unlimited_9bk9jtc8.wav")
 b = a.split_to_mono()  
 print(b)  
 print(b[0].channels ) 
+print(b[1].channels )
   
   
-b[0].export(out_f="outNow.wav",format="wav")
+b[0].export(out_f="mono1.wav",format="wav")
+b[1].export(out_f="mono2.wav",format="wav")
